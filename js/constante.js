a62b21c1f989d8730a7bf25e9c6e8bc8875cffbd
Andre da Silva Carmona <104648375+DevCarmona@users.noreply.github.com>
2023-02-22 11:59:58 -0300

    Constante

---
+++ js/constante.js
@@ -0,0 +1,6 @@
+    // const - Utilizado para nunca mudar seu valor
+// SyntaxError Missing initializer in const declaration
+//const nome; 
+// Nao pode alterar o valor definido - TypeError Assignment to constant variable
+const aula = "JS";
+aula = "JavaScript";
