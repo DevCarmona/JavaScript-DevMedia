2ff03c1cbd4e86dab0866c8106f0704cd33c5076
Andre da Silva Carmona <104648375+DevCarmona@users.noreply.github.com>
2023-02-22 14:39:46 -0300

    Operadores

---
+++ Variaveis e Operadores/Operadores/js/operadores.js
@@ -0,0 +1,46 @@
+    // Operadores Aritméticos
+// Operador módulo (%) - retorna o resto da divisao
+var resto1 = 15 % 4; 
+var resto2 = 3 % 1.2;
+console.log(resto1);
+console.log(resto2);
+// Verificar se o numero é par
+var verificarSePar = 20 % 2 == 0;
+console.log(verificarSePar);
+
+// Exemplos de conversão de tipos
+console.log(5 + 20); // 25
+console.log(5 + true); // 6
+console.log(5 + null); // 5
+console.log(true + true); // 2
+console.log(true + null); // 1
+console.log(null + null); // 0
+console.log(null + false); // 0
+console.log(5 + undefined); // NaN
+console.log(null + undefined); // NaN
+
+// Gerar valor e atribuir a msm variavel
+var preco = 10;
+preco = preco + 10;
+console.log(preco);
+// Outra forma de fazer
+var valor = 5;
+valor += 5;
+console.log(valor);
+
+    // Incremento e decremento
+var contador = 5;
+contador++;
+console.log(contador);
+contador--;
+console.log(contador);
+
+// operador sendo utilizado antes da variável, valor muda imediatamente
+var numero = 9;
+console.log(++numero);
+// operador sendo utilizado depois da variável, valor muda após operação atual ser executada
+var numero = 9;
+console.log(numero++);
+console.log("Valor da variavel: " + numero);
+
+    // Operadores relacionais
