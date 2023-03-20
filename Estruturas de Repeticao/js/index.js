cc7c5a66ecaefc850c47bf47e39d6a2a2ac46e40
Andre da Silva Carmona <104648375+DevCarmona@users.noreply.github.com>
2023-03-20 16:08:58 -0300

    Update index.js

--- Estruturas de Repeticao/js/index.js
+++ Estruturas de Repeticao/js/index.js
@@ -98,6 +98,7 @@ for ( fcont= 1 ; fcont <= parcelasTot ; fcont++ )
 
 // Loop com while - Decremento (contador sempre dentro do bloco)
 let lcont = 10;
+
 while ( lcont >= 0 )
 {
     console.log(lcont);
@@ -111,12 +112,14 @@ for ( fcont = 10 ; fcont >= 0 ; fcont--)
 // Estrutura repetição While - Percorrendo Array
 let paises = ["\n", "Brasil", "Suiça", "Inglaterra", "China"];
 let pcont = 0;
+
 while ( pcont < paises.length){
     console.log(paises[pcont]);
     pcont++;
 }
 // Estrutura repetição for - Percorrendo Array
 let country = ["\n", "Brasil","Argentina", "Croacia"];
+
 for (ccont = 0 ; ccont < country.length ; ccont++)
 {
     console.log(country[ccont]);
@@ -126,9 +129,10 @@ let numeros = [10, 5, 7, 8, 9, 6, 12, 4];
 let tot_impar = 0;
 let tot_par = 0;
 let cont = 0;
-while (cont < numeros.length)
+
+while ( cont < numeros.length )
 {
-    if (numeros[cont] % 2 == 0)
+    if ( numeros[cont] % 2 == 0 )
     {
         tot_par++;
     }
@@ -143,7 +147,8 @@ console.log("Total de impares é: " + tot_impar);
 //Estrutura repetição while - Percorrendo Array total alunos
 let alunos = ["\n", "Andre", "Joao", "Cezar", "Sergio"];
 let conta = 0;
-while (conta < alunos.length)
+
+while ( conta < alunos.length )
 {
     let aluno_online = alunos[conta];
     console.log(aluno_online);
@@ -151,8 +156,175 @@ while (conta < alunos.length)
 }
 //Estrutura repetição for - Percorrendo Array total alunos
 let alun = ["\n", "Andre", "Jorge", "Jaum", "Pedro"];
-for (contad = 0 ; contad < alun.length ; contad++)
+
+for ( contad = 0 ; contad < alun.length ; contad++ )
 {
     let alunos_online = alun[contad];
     console.log(alunos_online);
+}
+//  Estrutura de repetição for - Tabuada
+let multiplicador = 8;
+console.log("Tabuada do n° " + multiplicador);
+
+for ( let mcont = 0 ; mcont <= 10 ; mcont++)
+{
+    let resultado = multiplicador * mcont;
+    console.log(mcont + " x " + multiplicador + " = " + resultado);
+}
+//  Estrutura de repetição for - Meses
+let fmeses = ["\nJaneiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
+
+for ( let fcont = 0 ; fcont <= fmeses.length; fcont++)
+{
+    console.log(fmeses[fcont]);
+}
+//  Estrutura de repetição for - funcionarios com break
+//  Interrompe todo o laço e repetição
+let funcionarios = [
+    {id: 1, nome: "César", habilitado: false},
+    {id: 2, nome: "Jéssica", habilitado: false},
+    {id: 3, nome: "Renan", habilitado: true},
+    {id: 4, nome: "Marlon", habilitado: false},
+    {id: 5, nome: "Ana", habilitado: false},
+];
+let encontrouHabilitado = false;
+
+for ( let econt = 0 ; econt < funcionarios.length ; econt++ )
+{
+    let funcionario = funcionarios[econt];
+
+    if ( funcionario.habilitado == true)
+    {
+        console.log("Funcionario habilidato encontrado: " + funcionario.nome);
+        encontrouHabilitado = true;
+        break;
+    }
+}
+if ( !encontrouHabilitado )
+{
+    console.log("Nenhum funcionario habilitado encontrado");
+}
+//  Estrutura de repetição for - alunos com continue
+// interrompe o loop atual
+let falunos = [
+    {id: 1, nome: "Bruna", media: 8},
+    {id: 2, nome: "Laura", media: 7},
+    {id: 3, nome: "José", media: 5},
+    {id: 4, nome: "Guilherme", media: 4},
+    {id: 5, nome: "Rafael", media: 10},
+];
+
+for ( let fcont = 0 ; fcont < falunos.length ; fcont++ )
+{
+    let faluno = falunos[fcont];
+
+    if ( faluno < 6 )
+    {
+        continue;
+    }
+    console.log("\nId: " + faluno.id);
+    console.log("Nome: " + faluno.nome);
+    console.log("Media: " + faluno.media);
+}
+let notasTrimestrais = [7, 8, 6, 5]
+let somaNotas = 0;
+const quantidadeNotas = 4;
+
+for ( let ncont = 0 ; ncont < quantidadeNotas ; ncont++ )
+{
+    somaNotas = somaNotas + notasTrimestrais[ncont];
+}
+let media = somaNotas / quantidadeNotas;
+
+if ( media >= 7 )
+{
+    console.log("Aprovado, media trimestral: " + media);
+} else
+  {
+    console.log("Reprovado, media trimestral: " + media);
+  }
+//  Estrutura de repetição for
+//  Objeto: aluno ; Propriedades: nome, email e idade / Objeto: turma ; Propriedades: nome e ano.
+//  let aluno = { nome: "Marcos", email: "marcos@gmail.com", idade: 25 };
+//  let turma = { nome: "2130A", ano: 2021 };
+//  Exemplo Sem Propriedades usa colchete
+let exaluno = ["Marcos", "marcos@gmail.com", 25];
+let exturma = ["2130A", 2021]
+
+console.log(" ");
+console.log("Nome: " + exaluno[0]);
+console.log("Email: " + exaluno[1]);
+console.log("Idade: " + exaluno[2]);
+console.log("Turma: " + exturma[0]);
+console.log("Ano letivo: " + exturma[1]);
+//  Exemplo Sem Propriedades usa colchete
+let students = [
+    ["Marcos", "marcos@gmail.com", 25],
+    ["Andre", "andre@gmail.com", 38],
+    ["Bruna", "bruna@gmail.com", 43]
+];
+let classes = [
+    ["210A", 2020],
+    ["220C", 2022]
+]
+
+console.log("\nNome: " + students[0][0]);
+console.log("Email: " + students[0][1]);
+console.log("Idade: " + students[0][2]);
+console.log("Turma: " + classes[0][0]);
+console.log("Ano letivo: " + classes[0][1]);
+//  Exemplo com Propriedades usa chave
+let student = [
+    {nome: "Marcos", email: "marcos@gmail.com", idade: 25},
+    {nome: "Andre", email:"andre@gmail.com", idade: 38},
+    {nome: "Bruna", email: "bruna@gmail.com", idade: 43}
+];
+let classe = [
+    {nome: "215A", ano: 2022},
+    {nome: "213B", ano: 2023}
+
+];
+
+console.log("\nNome: " + student[2].nome);
+console.log("Email: " + student[2].email);
+console.log("Idade: " + student[2].idade);
+console.log("Turma: " + classe[1].nome);
+console.log("Ano letivo: " + classe[1].ano);
+//  Exemplo com Propriedades usa chave
+let paluno = {nome: "Andre", email: "carmona@gmail.com", idade: 34};
+let pturma = {nome: "2130A", ano: 2023};
+
+console.log(" ");
+console.log("Nome: " + paluno.nome);
+console.log("Email: " + paluno.email);
+console.log("Idade: " + paluno.idade);
+console.log("Turma: "+ pturma.nome);
+console.log("Ano letivo: " + pturma.ano);
+console.log("");
+//  Estrutura de repetição for..in - percorre o objeto.
+//  Itera cada propriedade de um objeto, seguindo a ordem que elas estao no objeto.
+//  dados - representa a propriedade do objeto no loop
+let student1 = {nome: "Andre", email: "andre@gmail.com", idade: 34};
+
+for ( let dados in student1 )
+{
+    console.log(student1[dados]);
+} console.log("");
+//  Estrutura de repetição for..in - Carro percorre o objeto.
+let car = {nome: "Celta", marca: "Chevrolet", preco: 10000, ano:2003};
+let percentageDiscount = 5; //  Desconto Percentual
+
+for ( let property in car)
+{
+    if ( property == "preco") // Condiçao que verifica se a propriedade do carro é igual a "preco"
+    {
+        let price = property;
+        let discount = ( car[price] * percentageDiscount ) / 100;
+        let newPrice = car[price] - discount;
+
+        console.log("novo preço: R$" + newPrice.toFixed(2));
+    } else
+        {
+            console.log(property + ": " + car[property]);
+        }
 }
